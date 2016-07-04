9b62fe9c3e4c33d00752a584b015d401aa2f77a0
Knut Kiesel <kiesel@cern.ch>
2016-07-04 17:32:10 +0200

    adding all single plots

--- makePlots.py
+++ makePlots.py
@@ -14,25 +14,7 @@ import math
 
 import ROOT
 import downloadViaJson
-
-def style():
-    ROOT.gStyle.SetOptStat(0)
-    ROOT.gROOT.SetBatch()
-    st = ROOT.gStyle
-    st.SetPadTopMargin(0.08)
-    st.SetPadBottomMargin(0.1)
-    st.SetPadLeftMargin(0.09)
-    st.SetPadRightMargin(0.00)
-    textSize = 0.05
-    st.SetLabelSize(textSize, "xyz")
-    st.SetTitleSize(textSize, "xyz")
-    st.SetTextFont(st.GetLabelFont())
-    st.SetTextSize(.8*st.GetLabelSize())
-    st.SetTitleOffset(1.1, "x")
-    st.SetTitleOffset(.9, "y")
-    st.SetPadTickX(1)
-    st.SetPadTickY(1)
-    ROOT.TGaxis.SetMaxDigits(6)
+import style
 
 class Parameter:
     name = ""
@@ -64,9 +46,13 @@ objects = [
     ("FPIX(x-,z+)", ROOT.kMagenta),
 ]
 
+plotDir = "/afs/cern.ch/user/k/kiesel/www/plots"
 
-def save(name):
-    ROOT.gPad.SaveAs("plots/{}.pdf".format(name))
+
+
+def save(name, folder="plots", endings=[".pdf"]):
+    for ending in endings:
+        ROOT.gPad.GetCanvas().SaveAs(os.path.join(folder,name+ending))
 
 def randomName():
     """
@@ -81,7 +67,7 @@ def runFromFilename(filename):
     m = re.match(".*Run(\d+).root", filename)
     if m:
         return int(m.group(1))
-    m2 = re.match(".*/Results(\d+)/.*", filename) # for pseudo
+    m2 = re.match(".*/Results(\d+)[^\d].*", filename) # for pseudo
     if m2:
         return int(m2.group(1))
     else:
@@ -120,6 +106,73 @@ def getInputHists(searchPath="root-files/Run*.root"):
         if newHists: hists[runNr] = newHists
     return sortedDict(hists)
 
+def exceedsCuts(h, cutDict=False):
+    maxErrCut = 10
+    sigCut = 2.5
+    maxCut = 200
+    var = h.GetName().split("_")[0]
+    cut = parDict[var].cut
+
+    binInfos = []
+    for bin in range(1,h.GetNbinsX()+1):
+        c = abs(h.GetBinContent(bin))
+        e = h.GetBinError(bin)
+        if c > maxCut or e > maxErrCut:
+            binInfos.append("fail")
+        elif c > cut and e and c/e > sigCut:
+            binInfos.append("update")
+        else:
+            binInfos.append("good")
+    if "fail" in binInfos:
+        return "fail"
+    elif "update" in binInfos:
+        return "update"
+    else:
+        return "good"
+
+def checkNewAlignment(hmap, cutDict):
+    infos = [ exceedsCuts(x, cutDict) for x in hmap.values() ]
+    return "fail" not in infos and "update" in infos
+
+def align(hmap, hmapAlignSettings):
+    hmapNew = {}
+    for name, h in hmap.iteritems():
+        hSet = hmapAlignSettings[name]
+        hmapNew[name] = h.Clone(h.GetName()+"_"+randomName())
+        for bin in range(1,7):
+            hmapNew[name].SetBinContent(bin,h.GetBinContent(bin)-hSet.GetBinContent(bin))
+    return hmapNew
+
+def drawHists(hmap, savename):
+    hnames = ["Xpos", "Ypos","Zpos", "Xrot", "Yrot", "Zrot"]
+    line = ROOT.TLine()
+    line.SetLineColor(ROOT.kRed)
+    c = ROOT.TCanvas(randomName(),"",1200,600)
+    c.Divide(3,2)
+    for ih, hname in enumerate(hnames):
+        c.cd(ih+1)
+        h = hmap[hname]
+        h.SetLineColor(ROOT.kBlack)
+        h.SetFillColor(ROOT.kGreen-7)
+        cutStatus = exceedsCuts(h)
+        if cutStatus == "update":
+            h.SetFillColor(ROOT.kOrange-9)
+        elif cutStatus == "fail":
+            h.SetFillColor(ROOT.kRed)
+        for bin in range(1,7):
+            h.GetXaxis().SetBinLabel(bin,objects[bin-1][0])
+        h.GetXaxis().SetRange(1,6)
+        h.GetYaxis().SetRangeUser(-50,50)
+        h.Draw("histe")
+        cut = h.GetBinContent(8)
+        if not cut:
+            cuts = {"Xpos":5, "Ypos":10, "Zpos":15, "Xrot":30, "Yrot":30, "Zrot":30}
+            cut = cuts[h.GetName().split("_")[0]]
+        line.DrawLine(0,-cut,6,-cut)
+        line.DrawLine(0,+cut,6,+cut)
+    c.cd(0)
+    save(savename, plotDir)
+
 def drawHistsVsRun(hmap, savename):
     if not hmap: return
     line = ROOT.TLine()
@@ -141,7 +194,7 @@ def drawHistsVsRun(hmap, savename):
     text.Draw()
     textCMS = ROOT.TLatex(.05,.96, "#font[61]{CMS} #scale[0.76]{#font[52]{Private Work}}")
     textCMS.Draw()
-    save(savename)
+    save(savename, plotDir, endings=[".pdf",".png"])
 
 def getHistsVsRun(inputHists, minRun=-1):
     inputHists = sortedDict(dict((key,value) for key, value in inputHists.iteritems() if key >= minRun))
@@ -164,8 +217,9 @@ def getHistsVsRun(inputHists, minRun=-1):
 def diffHistsVsRun(inputHists, inputHists2, minRun=-1):
     inputHists = sortedDict(dict((key,(value,None)) for key, value in inputHists.iteritems() if key >= minRun))
     for key, value in inputHists2.iteritems():
-        if key not in inputHists: inputHists[key] = (None,value)
-        else: inputHists[key][1] = value
+        if key not in inputHists: inputHists[key] = (None, value)
+        else: inputHists[key] = (inputHists[key][0], value)
+    inputHists = sortedDict(inputHists)
 
     hdefault = ROOT.TH1F("", ";;#Delta blub", len(inputHists), 0, len(inputHists))
     hdefault.SetLabelSize(.04)
@@ -173,7 +227,7 @@ def diffHistsVsRun(inputHists, inputHists2, minRun=-1):
         if len(inputHists) < 20 or not bin%int(len(inputHists)/20) or bin+1==len(inputHists):
             hdefault.GetXaxis().SetBinLabel(bin+1, str(runNr))
     histsVsRun = {}
-    for iRun, runNr, (hmap1, hmap2)  in enumerate(inputHists.iteritems()):
+    for iRun, (runNr, (hmap1, hmap2))  in enumerate(inputHists.iteritems()):
         if hmap1 and hmap2:
             for hname, h in hmap1.iteritems():
                 h2 = hmap2[hname]
@@ -185,19 +239,37 @@ def diffHistsVsRun(inputHists, inputHists2, minRun=-1):
                     e2 = h2.GetBinError(bin+1)
                     histsVsRun[hname][bin].SetBinContent(iRun+1, c-c2)
                     histsVsRun[hname][bin].SetBinError(iRun+1, math.sqrt(e**2+e2**2))
-        if hmap1 and not hmap2:
-            for hname, h in hmap1.iteritems():
-                if hname not in histsVsRun: histsVsRun[hname] = [ hdefault.Clone() for i in range(6) ]
-                for bin in range(6):
-                    histsVsRun[hname][bin].SetBinContent(iRun+1, 0)
-                    histsVsRun[hname][bin].SetBinError(iRun+1, 50)
-        if not hmap1 and hmap2:
-            for hname, h in hmap2.iteritems():
-                if hname not in histsVsRun: histsVsRun[hname] = [ hdefault.Clone() for i in range(6) ]
-                for bin in range(6):
-                    histsVsRun[hname][bin].SetBinContent(iRun+1, 0)
-                    histsVsRun[hname][bin].SetBinError(iRun+1, 50)
+#        if hmap1 and not hmap2: print "Run {} not in manual workflow".format(runNr)
+#        if not hmap1 and hmap2: print "Run {} not in online workflow".format(runNr)
     return histsVsRun
+
+def pseudoAlignment(inputHists, app=""):
+    referenceRun = -1
+    for run, hmap in inputHists.iteritems():
+        drawHists(hmap, "pre{}".format(run)+app)
+        if referenceRun > 0:
+            hmap = align(hmap,inputHists[referenceRun])
+        if checkNewAlignment(hmap):
+            print "updated alignment", run
+            referenceRun = run
+        drawHists(hmap, "post{}".format(run)+app)
+
+def cutParameterVariation(parName, cuts):
+    gr = ROOT.TGraph()
+    myCuts = copy.deepcopy(parDict)
+    for i, c in enumerate(cuts):
+        myCuts[parName].cut = c
+        n = len(getHistsVsRun(inputHists, True, myCuts)[1])
+        gr.SetPoint(i,c,n)
+    unit = "#mum" if "pos" in parName else "^{#circ}"
+    gr.SetTitle("{}; limit [{}];number of updates".format(parName,unit))
+    gr.SetMarkerStyle(20)
+    gr.Draw("ap")
+    l = ROOT.TLine()
+    origCut = parDict[parName].cut
+    yAxis = gr.GetHistogram().GetYaxis()
+    l.DrawLine(origCut,yAxis.GetXmin(),origCut,yAxis.GetXmax())
+    save("cutParameterVariation_"+parName)
 
 def updateFile(source, dest, changes={}):
     with open(source) as f:
@@ -205,55 +277,41 @@ def updateFile(source, dest, changes={}):
     with open(dest, "w") as f:
         f.write(x.safe_substitute(changes))
 
-def copyToWebSpace(files=[]):
-    dest = "/afs/cern.ch/user/k/kiesel/www/"
-    for f in files:
-        subprocess.call(["convert", "plots/{}.pdf".format(f), "-trim", "plots/{}.png".format(f)])
-        shutil.copyfile("plots/{}.pdf".format(f), dest+f+".pdf")
-        shutil.copyfile("plots/{}.png".format(f), dest+f+".png")
+def getNthLastRun(inputHists, N):
+    sortedRuns = sorted(inputHists.keys())
+    return sortedRuns[-min(N, len(sortedRuns))]
+
+def getTableString(runs):
+    return "\n".join(["<tr> <td>{0}</td> <td><a href=plots/Run{0}.pdf>pdf</a></td> <td><a href=plots/Run{0}_pseudo.pdf>pdf</a></td> </tr>".format(r) for r in runs])
 
 def main():
-    todayStr = datetime.date.today().isoformat()
     inputHists = getInputHists()
-    inputHistsPseudo = getInputHists("/afs/cern.ch/user/k/kiesel/public/pp3.8T_PCL_Alignment/Results*/MinBias_2016/PCL_SiPixAl_DQM.root")
-    sortedRuns = sorted(inputHists.keys())
-    firstNewRun = sortedRuns[-min(10, len(sortedRuns))]
+    inputHistsPseudo = getInputHists("/afs/cern.ch/user/j/jcastle/public/pp3.8T_PCL_Alignment/Results*/MinBias_2015/PCL_SiPixAl_DQM.root")
+    inputHistsPseudo.update(getInputHists("/afs/cern.ch/user/j/jschulte/public/pp3.8T_PCL_Alignment/Results*/MinBias_2016/PCL_SiPixAl_DQM.root"))
+    inputHistsPseudo.update(getInputHists("/afs/cern.ch/user/k/kiesel/public/pp3.8T_PCL_Alignment/Results*/MinBias_2016/PCL_SiPixAl_DQM.root"))
+    firstNewRun = getNthLastRun(inputHists, 10)
+    firstNewRunPseudo = getNthLastRun(inputHistsPseudo, 10)
+    firstNewRunBoth = min([firstNewRun, firstNewRunPseudo])
 
+    todayStr = datetime.date.today().isoformat()
     drawHistsVsRun(getHistsVsRun(inputHists), "pixAlignment_pcl_all_{}".format(todayStr))
     drawHistsVsRun(getHistsVsRun(inputHists, firstNewRun), "pixAlignment_pcl_newest_{}".format(todayStr))
     drawHistsVsRun(getHistsVsRun(inputHistsPseudo), "pixAlignment_pseudo_all_{}".format(todayStr))
-    drawHistsVsRun(getHistsVsRun(inputHistsPseudo, firstNewRun), "pixAlignment_pseudo_newest_{}".format(todayStr))
+    drawHistsVsRun(getHistsVsRun(inputHistsPseudo, firstNewRunPseudo), "pixAlignment_pseudo_newest_{}".format(todayStr))
     drawHistsVsRun(diffHistsVsRun(inputHists, inputHistsPseudo), "pixAlignment_diff_all_{}".format(todayStr))
-    drawHistsVsRun(diffHistsVsRun(inputHists, inputHistsPseudo, firstNewRun), "pixAlignment_diff_newest_{}".format(todayStr))
+    drawHistsVsRun(diffHistsVsRun(inputHists, inputHistsPseudo, firstNewRunBoth), "pixAlignment_diff_newest_{}".format(todayStr))
 
+    for run, hmap in inputHists.iteritems():
+        drawHists(hmap, "Run{}".format(run))
+    for run, hmap in inputHistsPseudo.iteritems():
+        drawHists(hmap, "Run{}_pseudo".format(run))
 
-
-def copyToWeb():
-    todayStr = datetime.date.today().isoformat()
-    updateFile(
-        "/afs/cern.ch/user/k/kiesel/www/indexTemplate.html",
-        "/afs/cern.ch/user/k/kiesel/www/index.html",
+    updateFile("indexTemplate.html", "/afs/cern.ch/user/k/kiesel/www/index.html",
         {"date": todayStr,
-         "f1": "pixAlignment_pcl_all_{}".format(todayStr),
-         "f2": "pixAlignment_pcl_newest_{}".format(todayStr),
-         "f3": "pixAlignment_pseudo_all_{}".format(todayStr),
-         "f4": "pixAlignment_pseudo_newest_{}".format(todayStr),
-         "f5": "pixAlignment_diff_all_{}".format(todayStr),
-         "f6": "pixAlignment_diff_newest_{}".format(todayStr),
-        }
-    )
-    copyToWebSpace([
-        "pixAlignment_pcl_all_{}".format(todayStr),
-        "pixAlignment_pcl_newest_{}".format(todayStr),
-        "pixAlignment_pseudo_all_{}".format(todayStr),
-        "pixAlignment_pseudo_newest_{}".format(todayStr),
-        "pixAlignment_diff_all_{}".format(todayStr),
-        "pixAlignment_diff_newest_{}".format(todayStr),
-        ]
-    )
+        "table": getTableString(list(set(inputHists.keys())|set(inputHistsPseudo.keys())))})
+
+
 
 if __name__ == "__main__":
-    downloadViaJson.downloadViaJson()
-    style()
+    #downloadViaJson.downloadViaJson()
     main()
-    copyToWeb()
