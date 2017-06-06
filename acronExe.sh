eb158d45cca9925cf89f03aa9f21273ca9aa2d2c
Knut Kiesel <kiesel@cern.ch>
2017-06-06 17:39:57 +0200

    updating cmssw version for run2

--- acronExe.sh
+++ acronExe.sh
@@ -9,7 +9,7 @@ export SCRAM_ARCH=slc6_amd64_gcc453 >> $logFile 2>&1
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch >> $logFile 2>&1
 source $VO_CMS_SW_DIR/cmsset_default.sh >> $logFile 2>&1
 
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_12 >> $logFile 2>&1
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_9_2_0 >> $logFile 2>&1
 eval `scramv1 runtime -sh` >> $logFile 2>&1
 cd /afs/cern.ch/user/k/kiesel/alignment/timeEvolution >> $logFile 2>&1
 python makePlots.py >> $logFile 2>&1
